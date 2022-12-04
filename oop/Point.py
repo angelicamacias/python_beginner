d9c01c1f7895ab969a4186b42da4cfa91f19bb90
Angelica Macias <angeliclasmaciasp@gmail.com>
2022-12-03 19:33:54 -0700

    update of code oop

--- oop/Point.py
+++ oop/Point.py
@@ -6,7 +6,7 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y 
-        print(self)
+        print(self) #we can see that we will get 4 results form this print 
 
 
 point1 = Point (10, 20)
@@ -15,8 +15,8 @@ point3 = Point (13, 23)
 point4 = Point (13, 23)
 
 number1 = int("2")
-print(type(point1)) #The result it's actually the name of the current script 
-print(point1)   #The result of this is "raro", but Python dosen't know what this is a pint object. 
+print(type(point1)) #The result is actually the name of the current script 
+print(point1)   #The result of this is weird, but Python dosen't know what this is a pint object. 
 #we need to give some special instuctions inside the class defenition for see the point object.
 #we have to create a new method here for pyhom can to display the inofrmation. 
 print(number1)
